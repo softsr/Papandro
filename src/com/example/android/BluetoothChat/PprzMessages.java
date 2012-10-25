9a4babe805cf76181e7c6eb177375ccd33c51f94
Sergey Krukowski <softsr@yahoo.de>
2012-10-25 17:58:21 +0200

    PprzMessages parse class added

---
+++ src/com/example/android/BluetoothChat/PprzMessages.java
@@ -0,0 +1,15 @@
+package com.example.android.BluetoothChat;
+
+import java.util.HashMap;
+
+public class PprzMessages {
+	
+	public HashMap parse_class_by_id(String pprz_class) {
+		HashMap by_id = new HashMap<Object, Object>();
+		return null;
+	}
+	
+	public HashMap parse_class_by_msg(String pprz_class) {
+		return null;
+	}
+}
